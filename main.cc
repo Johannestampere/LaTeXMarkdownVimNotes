5c6c4cac9f04a6de19c0ebe0fce6cd97a5d1688f
Johannes Tampere <tamperejohannes@gmail.com>
2025-06-02 12:00:32 -0400

    render html notebook

--- main.cc
+++ main.cc
@@ -20,9 +20,54 @@ void create() {
     system(cmd.c_str());
 }
 
-int main() {
+void renderNote(const string name) {
+    ifstream in{"./" + name + ".txt"};
+    // stores HTML code to render notebook
+    ofstream out("note.html");
 
-    create();
+    // write into the html file
+    out << R"(
+        <html>
+            <head>
+                <meta charset="UTF-8">
+                <title>Notebook</title>
+                <link rel="stylesheet" href="style.css" />
+            </head>
+            <body>
+                <div id="content">
+                    content
+                </div>
+
+                <script type="module" src="render.ts"></script>
+            </body>
+        </html>
+    )";
+
+    string line, block;
+
+    bool md = false, latex = false;
+
+    while (getline(in, line)) {
+        if (line == "<<") {
+            md = true;
+            block.clear();
+        } else if (line == ">>" && md = true) {
+            out << "<div class='markdown'>" << block << "</div>\n";
+            md = false;
+        } else if (line == "((") {
+            latex = true;
+            block.clear();
+        } else if (line == "))" && latex == true) {
+            out << "<div class='latex'>\\[" << block << "\\]</div>\n";
+            latex = false;
+        } else if (md || latex) {
+            block += line + "\n";
+        }
+    }
+
+}
+
+int main() {
 
     return 0;
 }
